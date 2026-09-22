b317a7604634c2e9c8a26fe16fe37e8c8de2bcb8
scott910512-source <scott910512@gmail.com>
2026-09-22 15:09:07 +0900

    Refresh PWA cache for guided workout flow

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="project100-v4";
+const CACHE="project100-v6";
 const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
