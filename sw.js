d96b9f6d538211a61a60ed6aa70b8743078e6686
scott910512-source <scott910512@gmail.com>
2026-09-22 12:39:57 +0900

    Add Project 100 fitness PWA: sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="project100-v3";
+const ASSETS=["./","./index.html","./app.js","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).then(function(){return self.skipWaiting();}));});
+self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
+self.addEventListener("fetch",function(e){if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(function(hit){return hit||fetch(e.request).then(function(r){const copy=r.clone();caches.open(CACHE).then(function(c){c.put(e.request,copy);});return r;}).catch(function(){return caches.match("./index.html");});}));});
